cb11ebc47fe2e12e21c8bb6dab7df31a9975355a
jp-irons <jon@irons.ws>
2024-12-14 12:35:50 +1000

    Adding AP

--- main/main.py
+++ main/main.py
@@ -1,22 +1,34 @@
-import sys
 # import micropython_ota
 import network
-import socket
-import requests
-import time
 import json
 import os
 import logging as logging
 import asyncio
+
+from microdot import Microdot, Response
+from microdot.utemplate import Template
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 wlan_sta = network.WLAN(network.STA_IF)
 
-class WiFiConfigManager:
-    """ This class handles OTA updates. It connects to the Wi-Fi, checks for updates, downloads and installs them."""
-    def __init__(self, wlan_filename ='WIFI_CONFIG.json'):
+
+async def connect_to(ssid, password):
+    logger.debug('Trying to connect to %s...' % ssid)
+    wlan_sta.connect(ssid, password)
+    for retry in range(100):
+        connected = wlan_sta.isconnected()
+        if connected:
+            print()
+            return connected
+        await asyncio.sleep(0.1)
+        print('.', end='')
+    print()
+
+
+class WiFiManager:
+    def __init__(self, wlan_filename='WIFI_CONFIG.json'):
         self.wlan_filename = wlan_filename
         self.wlan_attributes = None
         self.load()
@@ -40,19 +52,7 @@ class WiFiConfigManager:
             with open(self.wlan_filename, 'w') as f:
                 json.dump(self.wlan_attributes, f)
 
-    def connect_to(self, ssid, password):
-        logger.debug('Trying to connect to %s...' % ssid)
-        wlan_sta.connect(ssid, password)
-        for retry in range(100):
-            connected = wlan_sta.isconnected()
-            if connected:
-                print()
-                return connected
-            time.sleep(0.1)
-            print('.', end='')
-        print()
-
-    def connect(self):
+    async def connect(self):
         logger.debug('connect()')
         wifi_credentials = self.wlan_attributes['WIFI']
         wlan_sta.active(True)
@@ -63,19 +63,36 @@ class WiFiConfigManager:
         for wap_credentials in wifi_credentials:
             ssid = wap_credentials['SSID']
             password = wap_credentials['PASSWORD']
-            connected = self.connect_to(ssid, password)
+            connected = await connect_to(ssid, password)
             if connected:
-                logger.info('Connected to %s', ssid, wlan_sta.ifconfig())
+                logger.info('Connected to %s %s', ssid, wlan_sta.ifconfig())
                 return connected
             else:
                 logger.info('Failed to connect to ' + ssid)
         logger.debug('Need to set up AP and get credentials')
         return connected
 
-wifi_config = WiFiConfigManager()
 
-def test_config_manager():
-    wifi_config.connect()
+server = Microdot()
+Response.default_content_type = 'text/html'
+
+
+@server.route('/', methods=['GET', 'POST'])
+async def index(req):
+    name = None
+    if req.method == 'POST':
+        name = req.form.get('name')
+    return Template('index.html').render(name=name)
+
+
+wifi_manager = WiFiManager()
+
+async def run_app():
+    await wifi_manager.connect()
+    logger.debug('starting app')
+    await asyncio.gather(server.start_server(port=80))
+    logger.debug('app finished')
+
 
 def disconnect_wifi():
     wlan_sta.active(True)
@@ -83,31 +100,11 @@ def disconnect_wifi():
         return None
     wlan_sta.disconnect()
 
-
-def http_get(host='', path=''):
-    # url = 'https://1drv.ms/t/s!AgUtKRuWyBCtloRNgOC4MIRUXVGVAQ?e=IzeAwW'
-    # url = 'https://drive.google.com/file/d/14rSq2J_wrDV_qIotLTCFt5TPppJ2iQHk/view?usp=drive_link'
-    #url='https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
-    url = 'http://storagepi.local/ota'
-    path = '/ota/a.html'
-
-    print(url)
-    response = requests.get(url)
-    # Get response code
-    response_code = response.status_code
-    print(response_code)
-    # Get response content
-    response_content = response.content
-    print(response_content)
+def execute():
+    asyncio.run(run_app())
 
 
-def check_for_update():
-    # connect_wifi()
-    # update_to_latest_version()
-    pass
-
-
-def main():
+async def main():
     # connect_wifi()
     pass
 
