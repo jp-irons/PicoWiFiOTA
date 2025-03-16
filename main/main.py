a0527441f0d9c2ae6d48c99dea1cb5c22284937d
jp-irons <jon@irons.ws>
2025-03-16 14:23:14 +1000

    Phew server Wi-Fi refactoring

--- main/main.py
+++ main/main.py
@@ -94,7 +94,7 @@ class WiFiManager():
     async def connect_to(self, ssid, password):
         logger.debug('Trying to connect to %s...' % ssid)
         self.sta.connect(ssid, password)
-        for retry in range(50):
+        for retry in range(100):
             connected = self.sta.isconnected()
             if connected:
                 print()
